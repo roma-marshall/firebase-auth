b4ddc8f45140d4cffc85027f168683a6f665eb1d
roma_marshall <roma_marshall@outlook.com>
2024-04-09 11:01:55 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col min-h-screen mb-auto">
+    <Header />
+    <main class="flex justify-center container mx-auto mt-20 mb-auto">
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+
+</script>
