71d5c6a0f89d815004603ed9a61bf266a3bb0f86
roma_marshall <roma_marshall@outlook.com>
2024-04-09 11:03:54 +0200

    add mocked firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "secretapikeyfirebase",
+    authDomain: "auth.domain.firebaseapp.com",
+    projectId: "vue-app",
+    storageBucket: "vue-app-db.appspot.com",
+    messagingSenderId: "212636259258",
+    appId: "1:21777668592358:web:bb4172e493722ab7a3cb43"
+}
+
+export default firebaseConfig
